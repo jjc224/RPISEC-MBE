34b967e36735190eda1505c2dee66d067f17bb8a
Phizo <youngphizo@gmail.com>
2018-07-11 00:26:38 +1000

    Lab1C done.

--- lab01/lab1B.rb
+++
@@ -1,6 +0,0 @@
-#  0x08048c49 <+101>:	call   0x8048840 <__isoc99_scanf@plt>
-#  0x08048c4e <+106>:	mov    eax,DWORD PTR [esp+0x1c]
-#  0x08048c52 <+110>:	mov    DWORD PTR [esp+0x4],0x1337d00d
-#  0x08048c5a <+118>:	mov    DWORD PTR [esp],eax
-#  0x08048c5d <+121>:	call   0x8048a74 <test>
-
