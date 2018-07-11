24d977ab12ae6a9f0a99aa52f126e5f3b54eb6c8
Phizo <youngphizo@gmail.com>
2018-07-12 04:56:28 +1000

    Added flag as comment to source.

--- lab01/lab1C.sh
+++ lab01/lab1C.sh
@@ -3,7 +3,7 @@
 # Very simple crackme which just compares input against 0x149a (5274):
 # 	0x080486fe <+81>: cmp eax,0x149a 
 
-flag=/tmp/lab1B.pass
+flag=/tmp/lab1B.pass    # n0_str1ngs_n0_pr0bl3m
 
 (python -c 'print 0x149a'; echo "cat /home/lab1B/.pass > $flag") | /levels/lab01/lab1C
 && echo "[+] Passfile saved to $flag."
